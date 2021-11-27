361412f8605f63621fb57a84a5479b1a382892b8
Abdulloh Musayev <abdulloh_musayev@epam.com>
2021-11-27 15:12:19 +0500

    feat: implement some utils functions

---
+++ src/utils.js
@@ -0,0 +1,8 @@
+const validateId = (id) => id.match(/\/w+/) && id.length > 10;
+
+const validatePerson = (person) =>
+  person.id && person.name && !isNaN(person.age) && Array.isArray(person.hobbies);
+
+const messageObj = (message) => ({ message });
+
+module.exports = { validateId, validatePerson, messageObj };
