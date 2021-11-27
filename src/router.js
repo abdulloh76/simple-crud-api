535cd5c42db92060e47f4122bbadff28701a442d
Abdulloh Musayev <abdulloh_musayev@epam.com>
2021-11-27 13:11:19 +0500

    feat: add forgotten endpoint

--- src/router.js
+++ src/router.js
@@ -1,20 +1,22 @@
 const { getPeople } = require('./controllers/getPeople');
 const { getPerson } = require('./controllers/getPerson');
 const { postPerson } = require('./controllers/postPerson');
+const { updatePerson } = require('./controllers/updatePerson');
 const { deletePerson } = require('./controllers/deletePerson');
 const { noResponse } = require('./controllers/noResponse');
+const people = require('./models/people');
 
 const router = (req, res) => {
   if (req.method === 'GET' && req.url === '/person') {
     getPeople(req, res);
+  } else if (req.method === 'GET' && req.url.match(/\/person\/\w+/)) {
+    getPerson(req, res, req.url.split('/')[2]);
   } else if (req.method === 'POST' && req.url === '/person') {
     postPerson(req, res);
-  } else if (req.method === 'GET' && req.url.match(/\/person\/\w+/)) {
-    const id = req.url.split('/')[2];
-    getPerson(req, res, id);
+  } else if (req.method === 'PUT' && req.url.match(/\/person\/\w+/)) {
+    updatePerson(req, res, req.url.split('/')[2]);
   } else if (req.method === 'DELETE' && req.url.match(/\/person\/\w+/)) {
-    const id = req.url.split('/')[2];
-    deletePerson(req, res, id);
+    deletePerson(req, res, req.url.split('/')[2]);
   } else {
     noResponse(req, res);
   }
