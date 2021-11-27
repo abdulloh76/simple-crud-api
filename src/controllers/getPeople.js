d3da425701fc46e8707ee9cf3e10f786857907c0
Abdulloh Musayev <abdulloh_musayev@epam.com>
2021-11-27 17:00:51 +0500

    feat: implement controllers  getPeople & getPerson

--- src/controllers/getPeople.js
+++ src/controllers/getPeople.js
@@ -1,11 +1,16 @@
-const getPeople = (req, res) => {
-  try {
-    console.log('from getPeople');
-    // res.writeHead(200, { 'Content-type': 'application/json' });
-    // res.end(JSON.stringify(jsonFile));
-  } catch (error) {
-    console.log(error);
-  }
+const Person = require('../models/Person');
+const { messageObj } = require('../utils');
+
+const getPeople = async (req, res) => {
+  Person.findAll()
+    .then((people) => {
+      res.writeHead(200, { 'Content-type': 'application/json' });
+      res.end(JSON.stringify(people));
+    })
+    .catch((e) => {
+      res.writeHead(500, { 'Content-type': 'application/json' });
+      res.end(JSON.stringify(messageObj(e.message)));
+    });
 };
 
 module.exports = { getPeople };
--- src/controllers/getPerson.js
+++ src/controllers/getPerson.js
@@ -1,10 +1,25 @@
-const getPerson = (req, res, id) => {
-  try {
-    console.log('from getPerson', `id: ${id}`);
-    // res.writeHead(200, { 'Content-type': 'application/json' });
-    // res.end(JSON.stringify(jsonFile));
-  } catch (error) {
-    console.log(error);
+const Person = require('../models/Person');
+const { validateId, messageObj } = require('../utils');
+
+const getPerson = async (req, res, id) => {
+  if (validateId(id)) {
+    Person.findById(id)
+      .then((person) => {
+        if (person) {
+          res.writeHead(200, { 'Content-type': 'application/json' });
+          res.end(JSON.stringify(person));
+        } else {
+          res.writeHead(404, { 'Content-type': 'application/json' });
+          res.end(JSON.stringify(messageObj('nothing was found fot this id')));
+        }
+      })
+      .catch((e) => {
+        res.writeHead(500, { 'Content-type': 'application/json' });
+        res.end(JSON.stringify(messageObj(e.message)));
+      });
+  } else {
+    res.writeHead(400, { 'Content-type': 'application/json' });
+    res.end(JSON.stringify(messageObj('id is invalid')));
   }
 };
 
