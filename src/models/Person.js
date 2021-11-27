3049754d89dd164c7962081daa2de96e002341d8
Abdulloh Musayev <abdulloh_musayev@epam.com>
2021-11-27 18:52:34 +0500

    fix: update and remove methods

--- src/models/Person.js
+++ src/models/Person.js
@@ -8,11 +8,17 @@ const add = (person) => new Promise((res, rej) => res(people.push(person)));
 
 const update = (person) =>
   new Promise((res, rej) => {
-    const curId = people.find((el) => el.id === person.id);
-    res(people.splice(curId - 1, curId, person));
+    const curId = people.findIndex((el) => el.id === person.id);
+    people.splice(curId, 1, person);
+    res(people[curId]);
   });
 
-const remove = (id) => new Promise((res, rej) => res(people.filter((el) => el.id !== id)));
+const remove = (id) =>
+  new Promise((res, rej) => {
+    const curId = people.findIndex((el) => el.id === id);
+    people.splice(curId, 1);
+    res(people);
+  });
 
 module.exports = {
   findAll,
