e5e7978a76ba19b6fa4985a2f3a3af63fc6044b6
Abdulloh Musayev <abdulloh_musayev@epam.com>
2021-11-28 14:59:09 +0500

    fix: handling uuid for request handlers

--- src/router.js
+++ src/router.js
@@ -4,27 +4,22 @@ const { postPerson } = require('./controllers/postPerson');
 const { updatePerson } = require('./controllers/updatePerson');
 const { deletePerson } = require('./controllers/deletePerson');
 const { noResponse } = require('./controllers/noResponse');
-const people = require('./models/people');
+const { uuidRegex } = require('./utils');
 
 const router = (req, res) => {
-  if (req.method === 'GET' && req.url === '/person') {
+  if (req.method === 'GET' && req.url.match(/\/person\/?$/)) {
     getPeople(req, res);
-  } else if (req.method === 'GET' && req.url.match(/\/person\/\w+/)) {
+  } else if (req.method === 'GET' && req.url.match(`/person/${uuidRegex}\/?$`)) {
     getPerson(req, res, req.url.split('/')[2]);
-  } else if (req.method === 'POST' && req.url === '/person') {
+  } else if (req.method === 'POST' && req.url.match(`/person/${uuidRegex}\/?$`)) {
     postPerson(req, res);
-  } else if (req.method === 'PUT' && req.url.match(/\/person\/\w+/)) {
+  } else if (req.method === 'PUT' && req.url.match(`/person/${uuidRegex}\/?$`)) {
     updatePerson(req, res, req.url.split('/')[2]);
-  } else if (req.method === 'DELETE' && req.url.match(/\/person\/\w+/)) {
+  } else if (req.method === 'DELETE' && req.url.match(`/person/${uuidRegex}\/?$`)) {
     deletePerson(req, res, req.url.split('/')[2]);
   } else {
     noResponse(req, res);
   }
-
-  // const currentEndpoint = [req.method + req.url] || 'default';
-  // currentEndpoint.match();
-  // let redirectedFunc = endpoints.match[currentEndpoint];
-  // redirectedFunc(req, res);
 };
 
 module.exports = { router };
--- src/utils.js
+++ src/utils.js
@@ -3,4 +3,6 @@ const validatePerson = (person) =>
 
 const messageObj = (message) => ({ message });
 
-module.exports = { validatePerson, messageObj };
+const uuidRegex = '[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}';
+
+module.exports = { validatePerson, messageObj, uuidRegex };
